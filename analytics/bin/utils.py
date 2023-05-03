3a6bdf28e9eff41a189d5395189041aa27cbc8eb
MHM-Rahmati <131310803+MHM-Rahmati@users.noreply.github.com>
2023-05-03 14:58:56 +0330

    Adapting PEP8

--- analytics/bin/utils.py
+++ analytics/bin/utils.py
@@ -1,23 +1,20 @@
-from sqlalchemy import create_engine, Table, Column, Integer, String, Float, MetaData, text
-from sqlalchemy.exc import OperationalError
+# Standard lib --> Third parties lib --> Local lib
+from math import *
+from sys import exit
 from os import environ
 from time import sleep
 from datetime import datetime
-from math import *
-from sys import exit
+from sqlalchemy import create_engine, Table, Column, Integer, String, Float, MetaData, text
+from sqlalchemy.exc import OperationalError
 import numpy as np
 from .config import *
 
-def run():
-    # TODO
-    # to do all etl task together
-    pass
 
 # check if connection is ok
-def connection_check(CS="MYSQL_CS", max_retry=1):
+def check_connection(connection_string="MYSQL_CS", max_retry=1):
     for i in range(max_retry):
         try:
-            engine = create_engine(environ[CS], pool_pre_ping=True, pool_size=10)
+            engine = create_engine(environ[connection_string], pool_pre_ping=True, pool_size=10)
             return True, engine
         except OperationalError as e:
             sleep(0.1)
@@ -25,22 +22,23 @@ def connection_check(CS="MYSQL_CS", max_retry=1):
 
 
 # Create destination table if not exists, based on table description
-def table_existence(engine):
-    MD_Obj = MetaData()
-    # TO DO : "TBL_NAME/TBL_DESCRIPTION" should define as an argument
-    DE_Task_Table =Table('DE_Task_Table', MD_Obj,
-          Column('time', String(16), default='20230427 235959', primary_key=True),
-          Column('device_id', String(64), default='None', primary_key=True),
-          Column('total_points', Integer, default=0),
-          Column('max_temperature', Integer, default=0),
-          Column('total_distance', Float, default=0.0))
+def check_table_existence(engine):
+    md_obj = MetaData()
+    de_task_table = Table('de_task_table', md_obj,
+                          Column('time', String(16), default='20230427 235959', primary_key=True),
+                          Column('device_id', String(64), default='None', primary_key=True),
+                          Column('total_points', Integer, default=0),
+                          Column('max_temperature', Integer, default=0),
+                          Column('total_distance', Float, default=0.0)
+                          )
     try:
-        MD_Obj.create_all(engine)
-        return True, DE_Task_Table
+        md_obj.create_all(engine)
+        return True, de_task_table
     except Exception as e:
         return False, e
 
-# get results of given database
+
+# Get results of given database
 def get_results(engine, query):
     try:
         with engine.connect() as conn:
@@ -50,9 +48,44 @@ def get_results(engine, query):
         return False, e
 
 
+# Transform data to given form
+def transform(data):
+    data_dic = {}
+    res = []
+    for data_log in data:
+        if data_log[1] in data_dic.keys():
+            data_dic[data_log[1]].append(data_log)
+        else:
+            data_dic[data_log[1]] = [data_log]
+    current_time = datetime.now()\
+        .strftime('%x %X')\
+        .replace('/', '')\
+        .replace(':', '')
+    # walk through data per device per hour
+    for k, v in data_dic.items():
+        # sort to keep order of seen location -> guarantee the correct distance
+        v.sort()
+        max_temp = int(v[0][2])
+        total_dist = 0
+        data_points = len(v)
+        # Error Handling for devices with only one position
+        if data_points > 1:
+            for i in range(1, len(v)):
+                if int(v[i][2]) > max_temp:
+                    max_temp = int(v[i][2])
+                total_dist += get_distance(v[i - 1][3], v[i - 1][4], v[i][3], v[i][4])
+        hourly_device_data = {'device_id': str(k),
+                              'max_temperature': max_temp,
+                              'total_points': data_points,
+                              'total_distance': total_dist,
+                              'time': current_time
+                              }
+        res.append(hourly_device_data)
+    return res
+
+
 # insert into destination
-def insert(engine, table, data):
-    # data structure : (dev_id, max_temp, total_dist, currentTime)
+def insert(engine, table, data):  # data structure : (dev_id, max_temp, total_dist, currentTime)
     try:
         insert_obj = table.insert()
         with engine.connect() as conn:
@@ -68,8 +101,9 @@ def insert(engine, table, data):
         return True, None
     except Exception as e:
         return False, e
-        
 
+
+# Find distance between 2 points
 def get_distance(lat1, lon1, lat2, lon2):
     lat1 = float(lat1)
     lat2 = float(lat2)
@@ -81,40 +115,3 @@ def get_distance(lat1, lon1, lat2, lon2):
     a = np.sin(dlat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2) ** 2
     c = 2 * np.arcsin(np.sqrt(a))
     return 6371 * c
-
-def transform(data):
-    dataDic = {}
-    res = []
-    for datalog in data:
-        if datalog[1] in dataDic.keys():
-            dataDic[datalog[1]].append(datalog)
-        else:
-            dataDic[datalog[1]] = [datalog]
-
-    currentTime = datetime.now().strftime('%x %X').replace('/','').replace(':','')
-    
-    # walk through data per device per hour
-    for k, v in dataDic.items():
-        # sort to keep order of seen location -> guarantee the correct distance
-        v.sort()
-        max_temp = int(v[0][2])
-        total_dist = 0
-        data_points = len(v)
-        # Error Handling for devices with only one position
-        if data_points > 1:
-            for i in range(1, len(v)):
-                if int(v[i][2]) > max_temp:
-                    max_temp = int(v[i][2])
-                total_dist += get_distance(v[i-1][3], v[i-1][4], v[i][3], v[i][4])
-        hourlyDevRes = {'device_id': str(k), 'max_temperature': max_temp, 'total_points': data_points, 'total_distance': total_dist, 'time': currentTime}
-        res.append(hourlyDevRes)
-    return res
-
-def save2csv(data):
-    # TODO
-    currTime = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-    print(f'{OUTPUT_PATH}/output_{currTime}.csv')
-    with open(f'{OUTPUT_PATH}/output_{currTime}.csv', 'w') as ofile:
-        pass
-
-
